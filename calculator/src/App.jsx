4854b6b2909eb8774aac47bdd7e3820341b95959
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-03-01 17:37:55 +0530

    created components

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,37 +1,22 @@
 import { useState } from 'react'
-
-
-import './styles.css'
+import Wrapper from './components/Wrapper'
+import Screen from './components/Screen'
+import ButtonBox from './components/ButtonBox'
+import Button from './components/Button'
 
 function App() {
 
   return (
-
-    <div className='calcultor-grid'>
-      <div className='output'>
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
-      </div>
-      <button className='span-two'>AC</button>
-      <button>DEL</button>
-      <button>/</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>-</button>
-      <button>.</button>
-      <button>0</button>
-      <button className='span-two'>=</button>
-    </div>
-  
+  <div>
+    <Wrapper>
+      <Screen />
+        <ButtonBox>
+          <Button>
+            <h1>App</h1>
+          </Button>
+        </ButtonBox>
+    </Wrapper>
+  </div>
   )
 }
 
